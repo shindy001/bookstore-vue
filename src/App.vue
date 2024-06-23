63d7031fc40c444557acd2f85683ea9611dbb263
shindy001 <sindelar.p.etr@seznam.cz>
2024-06-23 22:36:29 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+        <nav>
+            <RouterLink to="/">Go to Home</RouterLink>
+        </nav>
+        <v-main>
+            <RouterView />
+        </v-main>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+    //
+</script>
---
+++ src/plugins/router.ts
@@ -0,0 +1,10 @@
+import { createMemoryHistory, createRouter } from 'vue-router'
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+const routes = [{ path: '/', component: HelloWorld }]
+
+export default createRouter({
+    history: createMemoryHistory(),
+    routes,
+})
