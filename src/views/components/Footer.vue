68f4bdc25c3c06f19c2e3d5d7c04182500f3cccb
shindy001 <sindelar.p.etr@seznam.cz>
2024-06-27 00:10:21 +0200

    feat: introduce layout component

---
+++ src/views/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <footer class="bg-black text-white shadow">
+        <div class="mx-auto w-full p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm sm:text-center"
+                >© 2024 <a href="#" class="hover:underline">BookStore™</a>. All Rights Reserved.
+            </span>
+            <ul class="mt-3 flex flex-wrap items-center text-sm font-medium sm:mt-0">
+                <li>
+                    <a href="#" class="me-4 hover:underline md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="me-4 hover:underline md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</template>
