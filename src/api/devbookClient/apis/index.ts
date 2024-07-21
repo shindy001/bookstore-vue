97397d2eb118de8dacf81c94fc90c3c6e3ed68b8
shindy001 <sindelar.p.etr@seznam.cz>
2024-07-21 18:06:44 +0200

    feat: generate devbook client

---
+++ src/api/devbookClient/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BookStoreModuleAuthorEndpointsApi';
+export * from './BookStoreModuleBookEndpointsApi';
+export * from './BookStoreModuleProductCategoryEndpointsApi';
+export * from './BookStoreModuleProductEndpointsApi';
+export * from './IdentityApi';
+export * from './SudokuModuleSudokuEndpointsApi';
+export * from './TimeTrackingModuleProjectEndpointsApi';
+export * from './TimeTrackingModuleWorkTaskEndpointsApi';
