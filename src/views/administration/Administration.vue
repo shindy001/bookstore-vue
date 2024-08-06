9bc6862609ea53ba82582404f38ab53579be359c
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-06 18:09:08 +0200

    refactor: simplify Administration page by using Sidenav

--- src/views/administration/Administration.vue
+++ src/views/administration/Administration.vue
@@ -1,70 +1,39 @@
 <template>
     <div class="flex h-full">
         <div class="bg-[#2f49d1] w-[300px] h-full text-[#8192e2] flex flex-col gap-2">
-            <RouterLink to="/" class="w-full my-4 h-16 text-white flex items-center justify-center">
+            <RouterLink to="/" class="my-4 h-16 text-white flex items-center justify-center">
                 <LibraryBig :size="32" class="inline mr-2" />
                 <span class="text-2xl font-semibold">BookStore</span>
             </RouterLink>
-            <div
-                class="pl-20 flex py-2 hover:bg-[#7788e0] hover:text-white cursor-pointer"
-                :class="activePage === Page.Dashboard ? 'bg-[#7788e0] text-white' : ''"
-                @click="() => setActivePage(Page.Dashboard)"
-            >
-                <House :size="24" class="inline mr-2" />
-                <span>Dashboard</span>
-            </div>
-            <div
-                class="pl-20 flex py-2 hover:bg-[#7788e0] hover:text-white cursor-pointer"
-                :class="activePage === Page.Products ? 'bg-[#7788e0] text-white' : ''"
-                @click="() => setActivePage(Page.Products)"
-            >
-                <PackageSearch :size="24" class="inline mr-2" />
-                <span>Products</span>
-            </div>
-            <div
-                class="pl-20 flex py-2 hover:bg-[#7788e0] hover:text-white cursor-pointer"
-                :class="activePage === Page.Invoices ? 'bg-[#7788e0] text-white' : ''"
-                @click="() => setActivePage(Page.Invoices)"
-            >
-                <Receipt :size="24" class="inline mr-2" />
-                <span>Invoices</span>
-            </div>
-            <div
-                class="pl-20 flex py-2 hover:bg-[#7788e0] hover:text-white cursor-pointer"
-                :class="activePage === Page.Messages ? 'bg-[#7788e0] text-white' : ''"
-                @click="() => setActivePage(Page.Messages)"
-            >
-                <MessageSquareMore :size="24" class="inline mr-2" />
-                <span>Messages</span>
-            </div>
+
+            <Sidenav @page-selected="(page: Page) => setActivePage(page)" />
         </div>
-        <component :is="pageComponentMap[activePage]" />
+        <div class="p-4">
+            <component :is="pageComponentMap[activePage]" />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue';
-    import { LibraryBig, PackageSearch, House, Receipt, MessageSquareMore } from 'lucide-vue-next';
+    import { LibraryBig } from 'lucide-vue-next';
     import Dashboard from './dashboard/Dashboard.vue';
-    import Products from './products/Products.vue';
+    import Overview from './products/Overview.vue';
+    import Categories from './products/Categories.vue';
     import Invoices from './invoices/Invoices.vue';
     import Messages from './messages/Messages.vue';
-
-    enum Page {
-        Dashboard,
-        Products,
-        Invoices,
-        Messages,
-    }
+    import Sidenav from '@/views/administration/_components/Sidenav.vue';
+    import { Page } from '@/views/administration/_common/types';
 
     const pageComponentMap = {
         [Page.Dashboard]: Dashboard,
-        [Page.Products]: Products,
+        [Page.ProductOverview]: Overview,
+        [Page.ProductCategories]: Categories,
         [Page.Invoices]: Invoices,
         [Page.Messages]: Messages,
     };
 
-    const activePage = ref(Page.Dashboard);
+    const activePage = ref<Page>(Page.Dashboard);
 
     async function setActivePage(page: Page) {
         activePage.value = page;
