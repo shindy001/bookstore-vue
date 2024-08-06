fd8de3abef8854b5176cc35245053d8f3d0716bc
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-06 18:08:22 +0200

    feat: introduce Sidenav component

---
+++ src/views/administration/_components/Sidenav.vue
@@ -0,0 +1,61 @@
+<template>
+    <SidenavItem
+        :item="{
+            name: 'Dashboard',
+            icon: 'pi-th-large',
+            isActive: () => activePage === Page.Dashboard,
+            onClick: () => setActivePage(Page.Dashboard),
+        }"
+    />
+    <SidenavItem
+        :item="{
+            name: 'Products',
+            icon: 'pi-tag',
+        }"
+        :submenuItems="[
+            {
+                name: 'Overview',
+                isActive: () => activePage === Page.ProductOverview,
+                onClick: () => setActivePage(Page.ProductOverview),
+            },
+            {
+                name: 'Categories',
+                isActive: () => activePage === Page.ProductCategories,
+                onClick: () => setActivePage(Page.ProductCategories),
+            },
+        ]"
+    />
+    <SidenavItem
+        :item="{
+            name: 'Invoices',
+            icon: 'pi-receipt',
+            isActive: () => activePage === Page.Invoices,
+            onClick: () => setActivePage(Page.Invoices),
+        }"
+    />
+    <SidenavItem
+        :item="{
+            name: 'Messages',
+            icon: 'pi-comments',
+            isActive: () => activePage === Page.Messages,
+            onClick: () => setActivePage(Page.Messages),
+        }"
+    />
+</template>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import SidenavItem from './SidenavItem.vue';
+    import { Page } from '@/views/administration/_common/types';
+
+    const activePage = ref(Page.Dashboard);
+
+    const emit = defineEmits<{
+        (e: 'page-selected', value: Page): void;
+    }>();
+
+    function setActivePage(page: Page) {
+        activePage.value = page;
+        emit('page-selected', page);
+    }
+</script>
