91e74c0993f8c29ede02be19267a0333a17145b6
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-06 17:38:53 +0200

    build: add primeicons to project

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,16 @@
  * Bootstraps plugins, layouts and then mounts the App`
  */
 
-import { registerPlugins } from '@/plugins'
+import { registerPlugins } from '@/plugins';
 import { registerLayouts } from '@/views/layouts/register';
 import { createApp } from 'vue';
 import App from './App.vue';
-import "./style.css";
+import './style.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 
 registerPlugins(app);
 registerLayouts(app);
 
-app.mount('#app')
+app.mount('#app');
