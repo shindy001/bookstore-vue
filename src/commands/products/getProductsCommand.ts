30672ab67426af5dc63cb63c53a00c31fee15255
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-08 14:30:42 +0200

    feat: introduce getProductsCommand

---
+++ src/commands/products/getProductsCommand.ts
@@ -0,0 +1,16 @@
+import { tryExecute } from '../utils';
+import { getProductsApi } from '@/plugins/devbookApiClient';
+
+export function createGetProductsCommand(onError?: (message: string) => void) {
+    const productsApi = getProductsApi();
+
+    return async (pageSize = 100, offset = 0, categoryId?: string) => {
+        const result = await tryExecute(() => productsApi.productsGetAll(pageSize, offset, categoryId));
+
+        if (result.success) {
+            return result.data;
+        }
+
+        onError?.('Uh oh, something went wrong.');
+    };
+}
