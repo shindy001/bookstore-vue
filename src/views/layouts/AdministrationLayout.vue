2f306aa840463554e008f82b2ef9d7eb6d454329
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-09 13:44:04 +0200

    refactor: extract Administration template content to AdministrationLayout

---
+++ src/views/layouts/AdministrationLayout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="min-h-screen grid grid-cols-1 grid-rows-1">
+        <div class="flex h-full">
+            <div class="bg-blue-700 min-w-[300px] h-full text-[#8192e2] flex flex-col gap-2">
+                <RouterLink to="/" class="my-4 h-16 text-white flex items-center justify-center">
+                    <LibraryBig :size="32" class="inline mr-2" />
+                    <span class="text-2xl font-semibold">BookStore</span>
+                </RouterLink>
+
+                <Sidenav />
+            </div>
+            <div class="p-4 bg-gray-100 w-full">
+                <slot></slot>
+            </div>
+        </div>
+        <Footer />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import { LibraryBig } from 'lucide-vue-next';
+    import Sidenav from '@/views/administration/_components/Sidenav.vue';
+    import Footer from '@/shared_components/Footer.vue';
+</script>
