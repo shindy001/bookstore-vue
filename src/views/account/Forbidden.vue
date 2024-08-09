101200f9c317b51089d4022cac6486f627eaecba
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-09 14:05:50 +0200

    fix: correct link on Forbidden page

--- src/views/account/Forbidden.vue
+++ src/views/account/Forbidden.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col justify-center">
         <h1 class="text-xl text-center">
             You don't have permission to access this page.
-            <RouterLink to="/" class="underline text-blue-700 hover:no-underline">Go Back.</RouterLink>
+            <RouterLink to="/" class="underline text-blue-700 hover:no-underline">Go back to BookStore.</RouterLink>
         </h1>
     </div>
 </template>
