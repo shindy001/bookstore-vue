3339e177f3505d8ffba0bf6855e12f33bdaf2485
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-12 12:00:03 +0200

    feat: add CreateProductCategoryDialog to ProductCategories page

--- src/views/administration/products/categories/ProductCategories.vue
+++ src/views/administration/products/categories/ProductCategories.vue
@@ -1,8 +1,13 @@
 <template>
     <PrimeCard class="shadow-none">
-        <template #title>Product Categories</template>
+        <template #title>
+            <div class="flex justify-between">
+                <span>Product Categories</span>
+                <PrimeButton label="Create Category" @click="() => (showCreateProductCategoryDialog = true)" />
+            </div>
+        </template>
         <template #content>
-            <PaginatedDataTable :getDataFn="getDataFn">
+            <PaginatedDataTable :key="productCategoriesTableKey" :getDataFn="getDataFn">
                 <template #columns>
                     <PrimeColumn field="name" header="Name" class="w-1/6"></PrimeColumn>
                     <PrimeColumn field="isTopLevelCategory" header="IsTopLevelCategory" class="w-1/5 sm:w-1/6">
@@ -15,17 +20,37 @@
             </PaginatedDataTable>
         </template>
     </PrimeCard>
+    <CreateProductCategoryDialog
+        :visible="showCreateProductCategoryDialog"
+        @onCreate="() => onCreateCategory()"
+        @onCancel="showCreateProductCategoryDialog = false"
+        @visibilityChanged="(value: boolean) => (showCreateProductCategoryDialog = value)"
+    />
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue';
     import { createGetProductCategoriesCommand } from '@/commands/products/getProductCategoriesCommand';
     import PaginatedDataTable from '@/views/administration/_components/PaginatedDataTable.vue';
+    import CreateProductCategoryDialog from './CreateProductCategoryDialog.vue';
+    import { useToastService } from '@/views/_shared/utils/toastHelper';
 
-    const error = ref('');
-    const getProductCategoriesCommand = createGetProductCategoriesCommand(
-        (errorMessage) => (error.value = errorMessage),
+    const productCategoriesTableKey = ref(0);
+    const showCreateProductCategoryDialog = ref(false);
+    const toastService = useToastService();
+    const getProductCategoriesCommand = createGetProductCategoriesCommand((errorMessage) =>
+        toastService.showError(`Error while fetching categories: ${errorMessage}`),
     );
 
     const getDataFn = (itemsPerPage: number, offset: number) => getProductCategoriesCommand(itemsPerPage, offset);
+
+    function onCreateCategory() {
+        toastService.showSuccess('Category successfully created.');
+        showCreateProductCategoryDialog.value = false;
+        refreshProductCategoriesTable();
+    }
+
+    function refreshProductCategoriesTable() {
+        productCategoriesTableKey.value += 1;
+    }
 </script>
