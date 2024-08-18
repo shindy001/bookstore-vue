32d2f27e0e71f704983fa353b3398a7610081541
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-18 23:00:38 +0200

    feat: add Delete ProductCategory action to ProductCategories

--- src/views/administration/products/categories/ProductCategories.vue
+++ src/views/administration/products/categories/ProductCategories.vue
@@ -16,30 +16,60 @@
                         </template>
                     </PrimeColumn>
                     <PrimeColumn field="subcategories" header="Subcategories" />
+                    <PrimeColumn header="Actions">
+                        <template #body="slotProps">
+                            <div class="flex gap-2">
+                                <!-- TODO - implement ProductCategory edit dialog -->
+                                <PrimeButton icon="pi pi-pencil" aria-label="Edit" outlined />
+                                <PrimeButton icon="pi pi-trash" aria-label="Delete" outlined @click="() => confirmCategoryDeletion(slotProps.data)" />
+                            </div>
+                        </template>
+                    </PrimeColumn>
                 </template>
             </PaginatedDataTable>
         </template>
     </PrimeCard>
     <CreateProductCategoryDialog
+        :key="productCategoriesTableKey"
         :visible="showCreateProductCategoryDialog"
         @onCreate="() => onCreateCategory()"
         @onCancel="showCreateProductCategoryDialog = false"
         @visibilityChanged="(value: boolean) => (showCreateProductCategoryDialog = value)"
+    />
+
+    <ConfirmationDialog
+        :visible="showDeleteCategoryConfirmationDialog"
+        :message="`Delete category ${categoryToDelete?.name}?`"
+        confirmButtonLabel="Delete"
+        @onConfirm="() => onCategoryDeleteConfirmation()"
+        @onCancel="showDeleteCategoryConfirmationDialog = false"
+        @visibilityChanged="(value: boolean) => (showDeleteCategoryConfirmationDialog = value)"
     />
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue';
     import { createGetProductCategoriesCommand } from '@/commands/products/getProductCategoriesCommand';
+    import { createDeleteProductCategoryCommand } from '@/commands/products/deleteProductCategoryCommand';
     import PaginatedDataTable from '@/views/administration/_components/PaginatedDataTable.vue';
     import CreateProductCategoryDialog from './CreateProductCategoryDialog.vue';
     import { useToastService } from '@/views/_shared/utils/toastHelper';
+    import ConfirmationDialog from '@/views/administration/_components/ConfirmationDialog.vue';
+    import { ProductCategoryDto } from '@/api/devbookClient';
+
+    const categoryToDelete = ref<ProductCategoryDto>();
+    const showDeleteCategoryConfirmationDialog = ref(false);
 
     const productCategoriesTableKey = ref(0);
+    const createProductCategoryDialogKey = ref(0);
     const showCreateProductCategoryDialog = ref(false);
     const toastService = useToastService();
     const getProductCategoriesCommand = createGetProductCategoriesCommand((errorMessage) =>
         toastService.showError(`Error while fetching categories: ${errorMessage}`),
+    );
+
+    const deleteProductCategoriesCommand = createDeleteProductCategoryCommand((errorMessage) =>
+        toastService.showError(`Error while deleting category: ${errorMessage}`),
     );
 
     const getDataFn = (itemsPerPage: number, offset: number) => getProductCategoriesCommand(itemsPerPage, offset);
@@ -50,7 +80,19 @@
         refreshProductCategoriesTable();
     }
 
+    function confirmCategoryDeletion(category: ProductCategoryDto) {
+        categoryToDelete.value = category;
+        showDeleteCategoryConfirmationDialog.value = true;
+    }
+
+    async function onCategoryDeleteConfirmation() {
+        await deleteProductCategoriesCommand(categoryToDelete.value?.id!).then(() => toastService.showSuccess("Category deleted."));
+        showDeleteCategoryConfirmationDialog.value = false;
+        refreshProductCategoriesTable();
+    }
+
     function refreshProductCategoriesTable() {
         productCategoriesTableKey.value += 1;
+        createProductCategoryDialogKey.value +=1;
     }
 </script>
