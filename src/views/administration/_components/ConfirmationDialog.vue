95a6997642b3f609d1157cc9a116215dd47b163b
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-18 22:59:45 +0200

    feat: introduce ConfirmationDialog component

---
+++ src/views/administration/_components/ConfirmationDialog.vue
@@ -0,0 +1,24 @@
+<template>
+    <PrimeDialog
+        modal
+        header="Confirmation"
+        :style="{ width: '35rem' }"
+        @update:visible="(visible: boolean) => $emit('visibilityChanged', visible)"
+    >
+        <p>{{ props.message }}</p>
+        <div class="flex justify-end gap-2">
+            <PrimeButton type="button" label="Cancel" severity="secondary" @click="$emit('onCancel')"></PrimeButton>
+            <PrimeButton type="button" :label="props.confirmButtonLabel" @click="$emit('onConfirm')"></PrimeButton>
+        </div>
+    </PrimeDialog>
+</template>
+
+<script setup lang="ts">
+    const props = withDefaults(defineProps<{
+        message: string,
+        confirmButtonLabel: string,
+    }>(), {
+        confirmButtonLabel: "Yes"
+    });
+
+</script>
