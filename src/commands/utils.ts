c2919dbd935df8edbd759f2ff302b5b8e0122cf6
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-20 19:55:11 +0200

    fix: correct tryExecute util method to correctly extract errors from axios error response

--- src/commands/utils.ts
+++ src/commands/utils.ts
@@ -13,10 +13,11 @@ export const tryExecute = async <T>(fn: () => Promise<AxiosResponse<T>>): Promis
         return { success: true, data: response.data, error: '' };
     } catch (error) {
         const axiosError = error as AxiosError;
+        const data = axiosError.response?.data as { errors: { [key: string]: string[] } };
         return {
             success: false,
             status: axiosError.response?.status,
-            error: axiosError.message ?? 'Uh oh. Something went wrong.',
+            error: data.errors ? JSON.stringify(data.errors) : 'Uh oh. Something went wrong.',
         };
     }
 };
