705e4edc30773b9130d85d03fa4b4896a1ec652f
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-20 20:02:00 +0200

    feat: add CreateBook button and dialog to Books page

--- src/views/administration/products/books/Books.vue
+++ src/views/administration/products/books/Books.vue
@@ -1,13 +1,19 @@
 <template>
     <PrimeCard class="shadow-none">
-        <template #title>Books</template>
+        <template #title>
+            <div class="flex justify-between">
+                <span>Books</span>
+                <PrimeButton label="Create Book" @click="() => (showCreateBookDialog = true)" />
+            </div>
+        </template>
         <template #content>
-            <PaginatedDataTable :getDataFn="getDataFn">
+            <PaginatedDataTable :getDataFn="getDataFn" :key="booksTableKey">
                 <template #columns>
                     <PrimeColumn field="imageUrl" class="w-[64px]">
                         <template #body="slotProps">
                             <img
-                            width="64" height="64" 
+                                width="64"
+                                height="64"
                                 :src="`https://img.icons8.com/wired/64/book.png`"
                                 :alt="slotProps.data.image"
                                 class="min-w-[64px] rounded"
@@ -27,14 +33,35 @@
             </PaginatedDataTable>
         </template>
     </PrimeCard>
+    <CreateBookDialog
+        :visible="showCreateBookDialog"
+        @onCreate="() => onCreateBook()"
+        @onCancel="showCreateBookDialog = false"
+        @visibilityChanged="(value: boolean) => (showCreateBookDialog = value)"
+    />
 </template>
 
 <script setup lang="ts">
     import { ref } from 'vue';
-    import { createGetProductsCommand } from '@/commands/products/getProductsCommand';
+    import { useGetProductsCommand } from '@/commands/products/getProductsCommand';
     import PaginatedDataTable from '@/views/administration/_components/PaginatedDataTable.vue';
+    import CreateBookDialog from './CreateBookDialog.vue';
+    import { useToastService } from '@/views/_shared/utils/toastHelper';
 
     const error = ref('');
-    const getProductsCommand = createGetProductsCommand((errorMessage) => (error.value = errorMessage));
-    const getDataFn = (itemsPerPage: number, offset: number) => getProductsCommand(itemsPerPage, offset, "Book");
+    const booksTableKey = ref(0);
+    const showCreateBookDialog = ref(false);
+    const toastService = useToastService();
+    const getProductsCommand = useGetProductsCommand((errorMessage) => (error.value = errorMessage));
+    const getDataFn = (itemsPerPage: number, offset: number) => getProductsCommand(itemsPerPage, offset, 'Book');
+
+    function onCreateBook() {
+        toastService.showSuccess('Book successfully created.');
+        showCreateBookDialog.value = false;
+        refreshBooksTable();
+    }
+
+    function refreshBooksTable() {
+        booksTableKey.value += 1;
+    }
 </script>
