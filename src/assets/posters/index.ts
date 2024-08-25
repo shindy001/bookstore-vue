fb905b131b4c04446ff2aa758580875f7b79f30f
shindy001 <sindelar.p.etr@seznam.cz>
2024-08-26 00:21:39 +0200

    feat: introduce poster assets

---
+++ src/assets/posters/index.ts
@@ -0,0 +1,17 @@
+import bestSellersDiscount20 from './20_bestsellers_discount.jpeg';
+import bookRankingsOfTheWeek from './book_rankings_of_the_week.jpeg';
+import ebookDiscount50 from './ebook_50_discount.jpeg';
+import freeShippingSunday from './free_shipping_sunday.jpeg';
+import summerReadingTips from './summer_reading_tips.jpeg';
+import giftToAnyOrderAbove30 from './gift_to_any_order_above_30.jpeg';
+import freeBookmarkToEveryOrder from './free_bookmark_to_every_order.jpeg';
+
+export {
+    bestSellersDiscount20,
+    bookRankingsOfTheWeek,
+    ebookDiscount50,
+    freeShippingSunday,
+    summerReadingTips,
+    giftToAnyOrderAbove30,
+    freeBookmarkToEveryOrder,
+};
