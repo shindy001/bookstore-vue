5d89fbb76248378c83bab6f9d607d8d7431f3800
shindy001 <sindelar.p.etr@seznam.cz>
2024-10-06 23:33:05 +0200

    feat: introduce stub Books and BookDetail pages

---
+++ src/views/books/BookDetail.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>This is a book {{ bookId }} detail</h1>
+</template>
+
+<script setup lang="ts">
+    import { useRoute } from 'vue-router';
+
+    const route = useRoute();
+    const bookId = route.params.id as string;
+</script>
---
+++ src/views/books/Books.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Books overview</h1>
+</template>
+
+<script setup lang="ts"></script>
