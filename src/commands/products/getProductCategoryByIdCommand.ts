32d4906b7f9608809d14db762f8f595a0a48d28a
shindy001 <sindelar.p.etr@seznam.cz>
2024-11-09 18:18:28 +0100

    feat: introduce getProductCategoryByIdCommand

---
+++ src/commands/products/getProductCategoryByIdCommand.ts
@@ -0,0 +1,16 @@
+import { tryExecute } from '../utils';
+import { getProductCategoriesApi } from '@/plugins/devbookApiClient';
+
+export function useGetProductCategoryByIdCommand(onError?: (message: string) => void) {
+    const productCategoriesApi = getProductCategoriesApi();
+
+    return async (id: string) => {
+        const result = await tryExecute(() => productCategoriesApi.productCategoriesGetById(id));
+
+        if (result.success) {
+            return result.data;
+        }
+
+        onError?.(result.error);
+    };
+}
