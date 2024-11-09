4a755b5eda67fb79b6b041febce88b7b8a479aea
shindy001 <sindelar.p.etr@seznam.cz>
2024-11-09 18:18:06 +0100

    fix: correct ProductDetail content size to max-w-screen-xl

--- src/views/products/ProductDetail.vue
+++ src/views/products/ProductDetail.vue
@@ -15,34 +15,36 @@
                 </div>
             </div>
         </div>
-        <div class="flex mx-auto max-w-[1200px] px-2 gap-24">
-            <div class="w-7/12 flex flex-col gap-4">
-                <p class="text-4xl font-bold">{{ product?.name }}</p>
-                <p v-if="product && (product as BookDto).author">
-                    Author: <span class="text-amber-700">{{ (product as BookDto)?.author }}</span>
-                </p>
-                <p>{{ product?.description }}</p>
-                <div class="flex flex-col">
-                    <div
-                        class="w-48 h-20 flex flex-col pt-2 content-center text-center relative top-[1px] z-[1] bg-ochr-100 border-x border-t border-gray-300 border-t-gray-300 rounded-t-md"
-                    >
-                        <p class="text-md font-bold"><BookMarked :size="18" class="inline mr-1 text-amber-700" /></p>
-                        <p class="text-md font-bold">{{ product?.productType }}</p>
-                    </div>
-                    <div class="p-8 w-full h-80 bg-ochr-100 border border-gray-300 rounded-b-md text-xl">
-                        <p class="font-semibold flex content-center align-middle">
-                            <p>Price in eshop</p>
-                            <p class="font-bold px-3 text-2xl">${{ product?.price }}</p>
-                            <div class="flex justify-center">
-                                <p class="line-through text-base font-thin text-amber-800 leading-7"
-                                >${{ product?.retailPrice }}</p>
-                            </div>
-                        </p>
+        <div>
+            <div class="flex mx-auto max-w-screen-xl px-4 gap-24">
+                <div class="w-7/12 flex flex-col gap-4">
+                    <p class="text-4xl font-bold">{{ product?.name }}</p>
+                    <p v-if="product && (product as BookDto).author">
+                        Author: <span class="text-amber-700">{{ (product as BookDto)?.author }}</span>
+                    </p>
+                    <p>{{ product?.description }}</p>
+                    <div class="flex flex-col">
+                        <div
+                            class="w-48 h-20 flex flex-col pt-2 content-center text-center relative top-[1px] z-[1] bg-ochr-100 border-x border-t border-gray-300 border-t-gray-300 rounded-t-md"
+                        >
+                            <p class="text-md font-bold"><BookMarked :size="18" class="inline mr-1 text-amber-700" /></p>
+                            <p class="text-md font-bold">{{ product?.productType }}</p>
+                        </div>
+                        <div class="p-8 w-full h-80 bg-ochr-100 border border-gray-300 rounded-b-md text-xl">
+                            <p class="font-semibold flex content-center align-middle">
+                                <p>Price in eshop</p>
+                                <p class="font-bold px-3 text-2xl">${{ product?.price }}</p>
+                                <div class="flex justify-center">
+                                    <p class="line-through text-base font-thin text-amber-800 leading-7"
+                                    >${{ product?.retailPrice }}</p>
+                                </div>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="w-5/12">
-                <img :src="product?.coverImageUrl ?? bookPlaceholder" />
+                <div class="w-5/12">
+                    <img :src="product?.coverImageUrl ?? bookPlaceholder" />
+                </div>
             </div>
         </div>
     </div>
